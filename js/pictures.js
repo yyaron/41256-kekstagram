d97f8bb384f8680cfd0ffba99d233173ed311100
yyaron <yaroslav.r.work@gmail.com>
2018-02-13 23:06:59 +0200

    #13 Личный проект: подробности - промежуточный результат

--- js/pictures.js
+++ js/pictures.js
@@ -48,7 +48,7 @@ var renderImage = function (picture) {
   var pictureTemplate = document.querySelector('#picture-template').content;
   //  копируем всё содержимое шаблона в новый элемент
   var pictureElement = pictureTemplate.cloneNode(true);
-  //  вставляем мгенерированную картинку из массива
+  //  вставляем cгенерированную картинку из массива
   pictureElement.querySelector('img').src = picture.url;
   //  вставляем рандомное число лайков из массива
   pictureElement.querySelector('.picture-likes').textContent = picture.likes;
@@ -67,11 +67,132 @@ for (var j = 0; j < friendPictures.length; j++) {
 }
 pictureList.appendChild(fragment);
 
-//  делаем видимым окно галереи
-var gallery = document.querySelector('.gallery-overlay');
-gallery.classList.remove('hidden');
+//  //////////////////  //
+//  ////// #13 ///////  //
+//  //////////////////  //
 
-//  заполняем окно данными с первой фотографии
-gallery.querySelector('.gallery-overlay-image').src = friendPictures[0].url;
-gallery.querySelector('.likes-count').textContent = friendPictures[0].likes;
-gallery.querySelector('.comments-count').textContent = friendPictures[0].comments;
+//  поле загрузки фото, окно предпросмотра фото, кнопка закрытия окна
+var uploadForm = document.querySelector('#upload-file');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFormClose = document.querySelector('.upload-form-cancel');
+
+//  var uploadOverlayOpen = function () {
+//    uploadOverlay.classList.remove('hidden');
+//  };
+
+//  var uploadOverlayClose = function () {
+//    uploadOverlay.classList.add('hidden');
+//  uploadForm.value = '';
+//  };
+
+//  показываем окно превью по изменению значения
+uploadForm.addEventListener('change', function () {
+  uploadOverlay.classList.remove('hidden');
+
+  //  закрываем по клику
+  uploadFormClose.addEventListener('click', function () {
+    uploadOverlay.classList.add('hidden');
+    uploadForm.value = '';
+  });
+
+  //  закрываем по нажатии на Escape
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      uploadOverlay.classList.add('hidden');
+      uploadForm.value = '';
+    }
+  });
+
+  //  закрываем по нажатии на Enter, если крестик в фокусе
+  uploadFormClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 13) {
+      uploadOverlay.classList.add('hidden');
+      uploadForm.value = '';
+    }
+  });
+});
+
+
+//  окно галереи, кнопка закрытия окна
+var gallery = document.querySelector('.gallery-overlay');
+var galleryClose = gallery.querySelector('.gallery-overlay-close')
+
+//  открываем окно по клику на фотографию
+document.addEventListener('click', function (evt) {
+  if (evt.target.tagName === 'IMG') {
+    evt.preventDefault();
+    var clickedItem = evt.target;
+    var itemStats = clickedItem.nextElementSibling.children;
+
+    //  делаем видимым окно галереи
+    gallery.classList.remove('hidden');
+
+    //  заполняем окно данными с выбранной фотографии
+    gallery.querySelector('.gallery-overlay-image').src = clickedItem.src;
+    gallery.querySelector('.likes-count').textContent = itemStats[0].textContent;
+    gallery.querySelector('.comments-count').textContent = itemStats[1].textContent;
+  }
+
+  //  закрываем по клику на крестик
+  galleryClose.addEventListener('click', function () {
+    gallery.classList.add('hidden');
+  });
+  //  закрываем по клику на Enter, если крестик в фокусе
+  galleryClose.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 13) {
+        gallery.classList.add('hidden');
+      }
+    });
+  //  закрываем по клику на Escape
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      gallery.classList.add('hidden');
+    }
+  });
+});
+
+//  кнопка увеличения, кнопка уменьшения, индикатор масштаба, фото
+var increaseButton = document.querySelector('.upload-resize-controls-button-inc');
+var decreaseButton = document.querySelector('.upload-resize-controls-button-dec');
+var sizeValue = document.querySelector('.upload-resize-controls-value');
+var imagePreview = document.querySelector('.effect-image-preview');
+
+//  функция увеличения масштаба фото
+var onIncreaseButtonClick = function () {
+  if ((parseInt(sizeValue.value)) < 100) {
+    sizeValue.value = (parseInt(sizeValue.value)) + 25;
+    imagePreview.style.transform = 'scale(' + (sizeValue.value / 100) + ')';
+    sizeValue.value += '%';
+  }
+};
+
+//  функция уменьшения масштаба фото
+var onDecreaseButtonClick = function () {
+  if ((parseInt(sizeValue.value)) > 25) {
+    sizeValue.value = (parseInt(sizeValue.value)) - 25;
+    imagePreview.style.transform = 'scale(' + (sizeValue.value / 100) + ')';
+    sizeValue.value += '%';
+  }
+};
+
+//  обработчик нажатия на кнопку увеличения
+increaseButton.addEventListener('click', onIncreaseButtonClick);
+//  обработчик нажатия на кнопку увеличения
+decreaseButton.addEventListener('click', onDecreaseButtonClick);
+
+//  ползунок слайдера
+var sliderPin = document.querySelector('.upload-effect-level-pin');
+
+//  обработчик взаимодействия с ползунком
+sliderPin.addEventListener('mouseup', function (evt) {
+  var windowWidth = window.innerWidth;
+  var sliderWidth = document.querySelector('.upload-effect-level-line').clientWidth;
+  var pinPosition = evt.clientX;
+
+  //  определяем положение ползунка на слайдере
+  var pinPositionOnSlider = pinPosition - ((windowWidth - sliderWidth) / 2);
+  //  определяем пропорцию эффекта относительно положения ползунка
+  var proportion = (pinPositionOnSlider / sliderWidth).toFixed(2);
+
+  console.log(proportion);
+});
